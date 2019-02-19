a2624bbd976c523770aafe3b322102e5386d1cd6
rebecabmfonseca <rebeca.bm.fonseca@gmail.com>
2019-02-19 19:48:02 -0300

    + classe Contato

--- lib/class/contato.dart
+++ lib/class/contato.dart
@@ -1,5 +1,11 @@
 import 'package:sqflite/sqflite.dart';
 
+final String idCol = "idColuna";
+final String nomeCol = "nomeColuna";
+final String emailCol = "emailColuna";
+final String telCol = "telefoneColuna";
+final String imgCol = "imagemColuna";
+
 class Contatos{
 
 }
@@ -11,5 +17,32 @@ class Contato{
   String email;
   String telefone;
   String imagem;
+
+  Contato.fromMap(Map map){
+    id = map[idCol];
+    nome = map[nomeCol];
+    email = map[emailCol];
+    telefone = map[telCol];
+    imagem = map[imgCol];
+  }
+
+  Map paraMap(){
+    Map<String, dynamic> map = {
+      nomeCol: nome,
+      emailCol:email,
+      telCol:telefone,
+      imgCol:imagem
+    };
+
+    if(id != null){
+      map[idCol] = id;
+    }
+    return map;
+  }
+
+  @override
+  String toString(){
+    return "Contato(id:$id, nome:$nome, email: $email, telefone: $telefone, img:$imagem";
+  }
 
 }
