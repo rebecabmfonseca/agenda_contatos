229d902a87e409e1331f042deb25ba703a5257b1
rebecabmfonseca <rebeca.bm.fonseca@gmail.com>
2019-02-19 20:51:46 -0300

    + estrutura do DB

--- lib/class/contato.dart
+++ lib/class/contato.dart
@@ -1,5 +1,8 @@
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'dart:async';
 
+final String tabContato = "contatoTabela";
 final String idCol = "idColuna";
 final String nomeCol = "nomeColuna";
 final String emailCol = "emailColuna";
@@ -7,7 +10,32 @@ final String telCol = "telefoneColuna";
 final String imgCol = "imagemColuna";
 
 class Contatos{
+  static final Contatos _instancia = Contatos.interno();
+  factory Contatos() => _instancia;
+  Contatos.interno();
 
+  Database _bd;
+
+  Future<Database> get bd async{
+    if(_bd != null){
+      return _bd;
+    }else{
+      _bd = await iniciarBD();
+      return _bd;
+    }
+  }
+
+  Future<Database> iniciarBD() async{
+    final caminhoBd = await getDatabasesPath();
+    final caminho = join(caminhoBd, "contatos.db");
+
+    return await openDatabase(caminho, version:1, onCreate: (Database bd, int novaVersao) async{
+      await bd.execute(
+        "CREATE TABLE $tabContato($idCol INTEGER PRIMARY KEY, $nomeCol TEXT, $emailCol TEXT, $telCol TEXT, $imgCol TEXT)"
+      );
+    });
+  }
+  
 }
 
 class Contato{
@@ -18,7 +46,7 @@ class Contato{
   String telefone;
   String imagem;
 
-  Contato.fromMap(Map map){
+  Contato.doMap(Map map){
     id = map[idCol];
     nome = map[nomeCol];
     email = map[emailCol];
